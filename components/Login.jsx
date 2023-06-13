99e73af6351b7ce41e7c5e5adc5d8fde57ac320b
Michael chinye <michaelchinye2018@gmail.com>
2023-06-13 02:13:48 +0100

    created new login screen

---
+++ components/Login.jsx
@@ -0,0 +1,55 @@
+import { View, Text, Image, TextInput, SafeAreaView, StyleSheet, Dimensions, Switch} from 'react-native'
+import React, {useState} from 'react'
+import { RectButton } from './Buttons'
+import image from '../assets/images/loginImg.png'
+import google from '../assets/images/Google.png'
+import facebook from '../assets/images/Facebook.png'
+import Apple from '../assets/images/AppleId.png'
+const Login = () => {
+     const [isEnabled, setIsEnabled] = useState(false);
+     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+     return (
+          <View>
+              <View>
+                    <Image source={image} resizeMode='cover' style={{width: Dimensions.get("screen").width, height: 250}} />
+              </View>
+              <Text style={{textAlign: "center", fontSize: 18, fontWeight: "800", paddingTop: 25, textTransform: 'uppercase'}} >Login</Text>
+              <View style={{marginTop: 25, paddingHorizontal: 20 }} >
+                    <TextInput placeholder='Email address' style={styles.textInput}  />
+                    <TextInput placeholder='Password' style={styles.textInput}  />
+                    <View style={{display:"flex", justifyContent:"space-between", alignItems: "center", flexDirection:"row"}}>
+                         <Switch trackColor={{false: '#767577', true: '#7B16FF'}}thumbColor={isEnabled ? '#3e3e3e' : '#f4f3f4'}ios_backgroundColor="#3e3e3e"onValueChange={toggleSwitch}value={isEnabled} />
+                         <Text>Forgot Password?</Text>
+                    </View>
+                    <RectButton title='Login' fontSize={20} />
+                    <View style={{marginTop: 20}}>
+                         <Text style={{textAlign: "center",textTransform: 'uppercase', fontSize: 20, fontWeight:"400"}}>OR</Text>
+                         <Text style={{textAlign: "center"}}>Login with</Text>
+                    </View>
+                    <View style={{marginTop: 15, display:"flex", justifyContent:"space-around", alignItems: "center", flexDirection:"row"}}>
+                         <Image source={google} style={{width: 40, height:40}} />
+                         <Image source={Apple} style={{width: 40, height:40}} />
+                         <Image source={facebook} style={{width: 40, height:40}} />
+                    </View>
+                    <Text style={{textAlign: "center", marginTop:10}}>Don't have an account? <Text style={{color: 'blue', textDecorationLine:"underline", }}>Register now</Text></Text>
+               </View>
+          </View>
+     )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+     
+     textInput:{
+          shadowColor: '#171717',
+          shadowOffset: {width: -2, height: 4},
+          shadowOpacity: 0.2,
+          shadowRadius: 3,
+          backgroundColor: 'white',
+          padding: 10,
+          marginBottom: 10,
+          borderBottomColor: "black",
+          borderBottomWidth: 0.5
+     }
+})
